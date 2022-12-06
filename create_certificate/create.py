b40304e352a85ef1b9967dba8ce24fbbb1b3f3b4
wesleySCOliveira <wesley.silva.cabral@gmail.com>
2022-12-05 23:15:25 -0300

    ajuste no email

--- create_certificate/create.py
+++ create_certificate/create.py
@@ -82,6 +82,6 @@ class Creat:
 
 
         smtp_config = {'host': os.environ['SMTP_SERVE'], 'port': os.environ['SMTP_PORT'], 'user': os.environ['SMTP_USER'], 'passwd':os.environ['SMTP_PASS']}
-        #e = Email(os.environ['SMTP_USER'],os.environ['SMTP_PASS'],file_name,['certidao2instancia@tjsp.jus.br',self._data['email']],smtp_config)
-        e = Email_enviar(os.environ['SMTP_USER'],os.environ['SMTP_PASS'],file_name,[self._data['email']],smtp_config)
+        e = Email_enviar(os.environ['SMTP_USER'],os.environ['SMTP_PASS'],file_name,['certidao2instancia@tjsp.jus.br',self._data['email']],smtp_config)
+        #e = Email_enviar(os.environ['SMTP_USER'],os.environ['SMTP_PASS'],file_name,[self._data['email']],smtp_config)
         e.send_email_ruralservice(path_out)
