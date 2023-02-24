a5f32eaad9bc43f4a5b0f7ea05e4dc9e15b8429d
wesleySCOliveira <wesley.silva.cabral@gmail.com>
2023-02-24 11:30:07 -0300

    ajuste

--- create_certificate/send_email.py
+++ create_certificate/send_email.py
@@ -16,40 +16,43 @@ class Email_enviar:
         self._pwd = pwd
 
     def send_email_ruralservice(self,path_temp):
-        print("send_email_ruralservice")
-        address = self._config['user']
-        passwd = self._config['passwd']
-        receiver = self._to
-        mail_content = """
-            Segue pdf com os dados para solicitação de certificado
-        """
-        message = MIMEMultipart()
-        message['From'] = address
-        message['To'] = ", ".join(receiver)
-        message['Subject'] = "Solicitação de Certidão"
-        message.attach(MIMEText(mail_content))
-        print(message)
-        # Anexando o PDF
-        pdfname=self._attach
-        fp=open(pdfname,'rb')
-        anexo = MIMEApplication(fp.read(),_subtype="pdf")
-        fp.close()
-        anexo.add_header('Content-Disposition','attachment',filename='certidao.pdf')
-        message.attach(anexo)
-        print('pdf anexado')
-        server = smtplib.SMTP('smtp.office365.com',587)
-        server.ehlo()
-        server.starttls()
-        server.ehlo()
-        print(self._login, self._pwd)
-        server.login(self._login, self._pwd)
-        print('server login')
-        text = message.as_string()
-        server.sendmail(address, receiver, text)
-        server.quit()
-        print('Email enviado')
-        time.sleep(2)
         try:
-            os.remove(path_temp+'certidao.pdf')
-        except:
-            pass
+            print("send_email_ruralservice")
+            address = self._config['user']
+            passwd = self._config['passwd']
+            receiver = self._to
+            mail_content = """
+                Segue pdf com os dados para solicitação de certificado
+            """
+            message = MIMEMultipart()
+            message['From'] = address
+            message['To'] = ", ".join(receiver)
+            message['Subject'] = "Solicitação de Certidão"
+            message.attach(MIMEText(mail_content))
+            print(message)
+            # Anexando o PDF
+            pdfname=self._attach
+            fp=open(pdfname,'rb')
+            anexo = MIMEApplication(fp.read(),_subtype="pdf")
+            fp.close()
+            anexo.add_header('Content-Disposition','attachment',filename='certidao.pdf')
+            message.attach(anexo)
+            print('pdf anexado')
+            server = smtplib.SMTP('smtp.office365.com',587)
+            server.ehlo()
+            server.starttls()
+            server.ehlo()
+            print(self._login, self._pwd)
+            server.login(self._login, self._pwd)
+            print('server login')
+            text = message.as_string()
+            server.sendmail(address, receiver, text)
+            server.quit()
+            print('Email enviado')
+            time.sleep(2)
+            try:
+                os.remove(path_temp+'certidao.pdf')
+            except:
+                pass
+        except Exception as e:
+            print(e)
