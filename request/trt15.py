5dbcd1ef40618bee11b14b80c882f6fa4902a7b0
wesleySCOliveira <wesley.silva.cabral@gmail.com>
2023-08-18 15:05:53 -0300

    ajustando para fechar aba

--- request/trt15.py
+++ request/trt15.py
@@ -29,107 +29,113 @@ class Trt15:
             self.print_colored("Diretório Criado!", "green")
         
     def login(self):
-        print('login')
-        time.sleep(2)
-        cookie = self.navegation.cookies()
+        try:
+            print('login')
+            time.sleep(2)
+            cookie = self.navegation.cookies()
 
-        url = "https://ceat.trt15.jus.br/ceat/certidaoAction.seam"
+            url = "https://ceat.trt15.jus.br/ceat/certidaoAction.seam"
 
-        payload={}
-        headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
-        'Cookie': 'JSESSIONID={}'.format(cookie[0]['value'])
-        }
+            payload={}
+            headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+            'Cookie': 'JSESSIONID={}'.format(cookie[0]['value'])
+            }
 
-        response = requests.request("GET", url, headers=headers, data=payload)
+            response = requests.request("GET", url, headers=headers, data=payload)
 
-        # URL para obter a imagem do captcha
-        url_captcha = "https://ceat.trt15.jus.br/ceat/seam/resource/captcha?f=1690828042005"
+            # URL para obter a imagem do captcha
+            url_captcha = "https://ceat.trt15.jus.br/ceat/seam/resource/captcha?f=1690828042005"
 
-        # Headers necessários para a solicitação
-        headers = {
-            'authority': 'ceat.trt15.jus.br',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'accept-language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
-            'cache-control': 'max-age=0',
-            'cookie': 'JSESSIONID={}; _ga=GA1.1.2084834512.1690570491; _ga_HMMN3T1GVL=GS1.1.1690573933.2.1.1690575809.0.0.0'.format(cookie[0]['value']),
-            'sec-ch-ua': '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Linux"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'none',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
-        }
+            # Headers necessários para a solicitação
+            headers = {
+                'authority': 'ceat.trt15.jus.br',
+                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'accept-language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+                'cache-control': 'max-age=0',
+                'cookie': 'JSESSIONID={}; _ga=GA1.1.2084834512.1690570491; _ga_HMMN3T1GVL=GS1.1.1690573933.2.1.1690575809.0.0.0'.format(cookie[0]['value']),
+                'sec-ch-ua': '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Linux"',
+                'sec-fetch-dest': 'document',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-site': 'none',
+                'sec-fetch-user': '?1',
+                'upgrade-insecure-requests': '1',
+                'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
+            }
 
-        # Fazer a solicitação para obter a imagem do captcha
-        response_captcha = requests.get(url_captcha, headers=headers)
+            # Fazer a solicitação para obter a imagem do captcha
+            response_captcha = requests.get(url_captcha, headers=headers)
 
-        # Salvar a imagem em um arquivo local
-        with open(os.path.join(self._data['path'],'captcha.jpg'), 'wb') as f:
-            f.write(response_captcha.content)
+            # Salvar a imagem em um arquivo local
+            with open(os.path.join(self._data['path'],'captcha.jpg'), 'wb') as f:
+                f.write(response_captcha.content)
 
-        print("Imagem do captcha salva com sucesso!")
-        response = self._captcha.resolve_normal(os.path.join(self._data['path'],'captcha.jpg'))
-        time.sleep(1)
-        print('passo pelo captcha')
-        if response is None:
+            print("Imagem do captcha salva com sucesso!")
             response = self._captcha.resolve_normal(os.path.join(self._data['path'],'captcha.jpg'))
-        #response = ''
-        response = response.replace('=','')
-        if '+' in response:
-            soma = int(response.split('+')[0]) + int(response.split('+')[1])
-        elif 'x' in response:
-            soma = int(response.split('x')[0]) + int(response.split('x')[1])
-        else:
-            soma = int(response)
-        soma = str(soma)
-        os.system('rm {}'.format(os.path.join(self._data['path'],'captcha.jpg')))
-        # URL para gerar a certidão
-        url_certidao = "https://ceat.trt15.jus.br/ceat/certidaoAction.seam"
+            time.sleep(1)
+            print('passo pelo captcha')
+            if response is None:
+                response = self._captcha.resolve_normal(os.path.join(self._data['path'],'captcha.jpg'))
+            #response = ''
+            response = response.replace('=','')
+            if '+' in response:
+                soma = int(response.split('+')[0]) + int(response.split('+')[1])
+            elif 'x' in response:
+                soma = int(response.split('x')[0]) + int(response.split('x')[1])
+            else:
+                soma = int(response)
+            soma = str(soma)
+            os.system('rm {}'.format(os.path.join(self._data['path'],'captcha.jpg')))
+            # URL para gerar a certidão
+            url_certidao = "https://ceat.trt15.jus.br/ceat/certidaoAction.seam"
 
-        # Dados do formulário para gerar a certidão (incluindo a resposta do captcha)
-        payload_certidao = {
-            'certidaoActionForm': 'certidaoActionForm',
-            'certidaoActionForm:decTipoPesquisa:radiotipopesquisa': 'D',
-            'certidaoActionForm:j_id23:doctoPesquisa': '{}'.format(self._data['cpf']),
-            'certidaoActionForm:j_id33:nomePesquisa': '',
-            'certidaoActionForm:j_id51:verifyCaptcha': soma,
-            'certidaoActionForm:certidaoActionEmitir': 'Emitir Certidão',
-            'javax.faces.ViewState': 'j_id1'
-        }
+            # Dados do formulário para gerar a certidão (incluindo a resposta do captcha)
+            payload_certidao = {
+                'certidaoActionForm': 'certidaoActionForm',
+                'certidaoActionForm:decTipoPesquisa:radiotipopesquisa': 'D',
+                'certidaoActionForm:j_id23:doctoPesquisa': '{}'.format(self._data['cpf']),
+                'certidaoActionForm:j_id33:nomePesquisa': '',
+                'certidaoActionForm:j_id51:verifyCaptcha': soma,
+                'certidaoActionForm:certidaoActionEmitir': 'Emitir Certidão',
+                'javax.faces.ViewState': 'j_id1'
+            }
 
-        # Fazer a solicitação para gerar a certidão
-        response_certidao = requests.post(url_certidao, headers=headers, data=payload_certidao)
+            # Fazer a solicitação para gerar a certidão
+            response_certidao = requests.post(url_certidao, headers=headers, data=payload_certidao)
 
-        print("Certidão gerada com sucesso!")
+            print("Certidão gerada com sucesso!")
 
-        url_pdf = "https://ceat.trt15.jus.br/ceat/certidaoAction.seam"
+            url_pdf = "https://ceat.trt15.jus.br/ceat/certidaoAction.seam"
 
-        # Dados do formulário para baixar o arquivo em PDF
-        payload_pdf = {
-            'certidaoActionForm': 'certidaoActionForm',
-            'certidaoActionForm:certidaoActionImprimir': 'Imprimir Certidão',
-            'javax.faces.ViewState': 'j_id2'
-        }
+            # Dados do formulário para baixar o arquivo em PDF
+            payload_pdf = {
+                'certidaoActionForm': 'certidaoActionForm',
+                'certidaoActionForm:certidaoActionImprimir': 'Imprimir Certidão',
+                'javax.faces.ViewState': 'j_id2'
+            }
 
-        # Fazer a solicitação para baixar o arquivo em PDF
-        response_pdf = requests.post(url_pdf, headers=headers, data=payload_pdf)
+            # Fazer a solicitação para baixar o arquivo em PDF
+            response_pdf = requests.post(url_pdf, headers=headers, data=payload_pdf)
 
-        if response_pdf.headers.get("content-type") == "application/pdf":
-            # Salvar o conteúdo do PDF em um arquivo local
-            with open(os.path.join(self._data['path'],'14- TRT15ª.pdf'), 'wb') as f:
-                f.write(response_pdf.content)
+            if response_pdf.headers.get("content-type") == "application/pdf":
+                # Salvar o conteúdo do PDF em um arquivo local
+                with open(os.path.join(self._data['path'],'14- TRT15ª.pdf'), 'wb') as f:
+                    f.write(response_pdf.content)
+                self.navegation.close_driver()
+            
+            for arquivo in os.listdir(self._data['path']):
+                if arquivo.find('14- TRT15ª.pdf') > -1:
+                    print('Download concluido para o cpf {}'.format(self._data['cpf']))
+                    return
             self.navegation.close_driver()
-        
-        for arquivo in os.listdir(self._data['path']):
-            if arquivo.find('14- TRT15ª.pdf') > -1:
-                print('Download concluido para o cpf {}'.format(self._data['cpf']))
-                return
-        self.navegation.close_driver()
-        raise ValueError
+            raise ValueError
+        except:
+            self.navegation.close_driver()
+            raise ValueError
+    
+    
 
         
 
